f21a0023feb1d411bfbf7da925f33c9a5fb9c2af
PatForAll <siekmeier.p@gmail.com>
2021-07-25 21:41:12 +0200

    create UnitOfMeasure and link from Ingredient to UOM

--- src/main/java/guru/springframework/sfg_recipe_project/domain/Ingredient.java
+++ src/main/java/guru/springframework/sfg_recipe_project/domain/Ingredient.java
@@ -13,8 +13,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //TODO: add UOM
-    //private UnitOfMeasure unitOfMeasure;
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
 
     @ManyToOne
     private Recipe recipe;
@@ -41,6 +41,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 
     public Recipe getRecipe() {
