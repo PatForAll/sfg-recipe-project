dc4477200a3fed86254a856fdaaae8538cc34375
PatForAll <siekmeier.p@gmail.com>
2021-10-19 13:30:48 +0200

    add JUnit test for Recipes Controller

--- src/test/java/guru/springframework/sfg_recipe_project/controllers/RecipesControllerTest.java
+++ src/test/java/guru/springframework/sfg_recipe_project/controllers/RecipesControllerTest.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfg_recipe_project.controllers;
 
+import guru.springframework.sfg_recipe_project.domain.Recipe;
 import guru.springframework.sfg_recipe_project.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -27,10 +32,23 @@ class RecipesControllerTest {
 
     @Test
     void getRecipesList() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(7L);
+        recipes.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String result = recipesController.getRecipesList(model);
 
         assertEquals("recipes/list", result);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
